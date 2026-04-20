604530d1b2d15f234dc3fe67481f1cd046399d8d
Ganesh.H <hganesh1909@gmail.com>
2026-04-20 18:31:58 +0530

    Update authController

--- Server/models/user.js
+++ Server/models/user.js
@@ -35,3 +35,19 @@ const userSchema = new mongoose.Schema({
     default: null,
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
